170d991b427163b10115e30330c0bfd5ffc61158
向文可 <13202090601@163.com>
2023-06-15 19:04:11 +0800

    refactor: 重构异常类型

--- src/error.ts
+++ src/error.ts
@@ -1,6 +1,16 @@
+export enum DaoErrorType {
+    NO_SUCH_TABLE = 100,
+}
 export class DaoError extends Error {
-    constructor(err: string, cause?: unknown) {
-        super('[DaoError] ' + err);
-        this.cause = cause;
+    code: DaoErrorType;
+    constructor(type: DaoErrorType, message: string, cause?: unknown) {
+        super(`[${type}] ${message}`, { cause });
+        this.code = type;
+        this.message = message;
+    }
+}
+export class NoSuchTableError extends DaoError {
+    constructor(name: string, cuase?: unknown) {
+        super(DaoErrorType.NO_SUCH_TABLE, '数据表不存在：' + name, cuase);
     }
 }
