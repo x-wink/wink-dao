00d49b414bfe5557cc09e84b9ae1934a51e2a585
向文可 <13202090601@163.com>
2023-06-15 19:05:03 +0800

    perf: 重命名工具函数，添加示例注释

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,11 @@
-export const convertFieldName = (name: string) => {
+/**
+ * 大小驼峰转下划线分割
+ * @param name
+ * @example camel2underline("createDate") === "create_date"
+ * @example camel2underline("UpdateDate") === "update_date"
+ * @returns
+ */
+export const camel2underline = (name: string) => {
     return name.replace(/[A-Z]/g, (sub, index) => {
         return (index ? '_' : '') + sub.toLowerCase();
     });
