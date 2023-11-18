412a07c6cd289d90cc602b5b15c5188a06f1fc13
向文可 <13202090601@163.com>
2023-11-18 15:01:18 +0800

    feat: CURD测试用例

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default (configEnv: ConfigEnv) => {
                 dir: './tests',
                 logHeapUsage: true,
                 coverage: {
-                    reporter: ['json'],
+                    reporter: ['html'],
                 },
             },
         })
